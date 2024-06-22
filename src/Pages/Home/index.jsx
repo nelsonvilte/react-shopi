8dbe023b78e10628e4738cc88a45db26141d06e9
nelsonvilte <nelsonvilte@gmail.com>
2024-06-22 01:49:14 -0300

    add consuming the fakestore API, for painting cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,15 +1,26 @@
-import Card from '../../Components/Card'
-import Layout from '../../Components/Layout'
+import { useState, useEffect } from "react";
+import Card from "../../Components/Card";
+import Layout from "../../Components/Layout";
+
 function Home() {
+  const [items, setItems] = useState(null);
 
-    return (
-        <Layout>
-            Home
-            <Card/>
-        </Layout>
-        
-    )
-  }
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
 
-  export default Home
-  
+  return (
+    <Layout>
+      Home
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card key={item.id} data={item} />
+        ))}
+      </div>
+    </Layout>
+  );
+}
+
+export default Home;
