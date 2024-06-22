fef3769bead35a348fcf8bf225fab2cf56e3e1da
nelsonvilte <nelsonvilte@gmail.com>
2024-06-22 19:46:52 -0300

    update show product in Product Detail component and new url APIRest

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -2,12 +2,18 @@ import { createContext, useState } from "react";
 
 export const ShoppingCartContext = createContext()
 
+// eslint-disable-next-line react/prop-types
 export const ShoppingCartProvider = ({children})=>{
-
+    // shopping Cart- Increment quantity
     const [count, setCount]=useState(0)
+
+    // product detail - open/close
     const [isProductDetailOpen, setIsProductDetailOpen]=useState(false)
     const openProductDetail=()=>setIsProductDetailOpen(true)
     const closeProductDetail=()=>setIsProductDetailOpen(false)
+
+    // producto detail - show product
+    const [productToShow, setProductToShow]=useState({})
 
     return(
         <ShoppingCartContext.Provider value={{
@@ -15,7 +21,9 @@ export const ShoppingCartProvider = ({children})=>{
             setCount,
             openProductDetail,
             closeProductDetail,
-            isProductDetailOpen
+            isProductDetailOpen,
+            productToShow, 
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>
