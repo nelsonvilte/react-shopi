08383fde67cc670c6a7189fb74738df67f9cbdd2
nelsonvilte <nelsonvilte@gmail.com>
2024-06-22 02:06:02 -0300

    add global context of application

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,4 +1,5 @@
 import { useRoutes, BrowserRouter } from "react-router-dom";
+import { ShoppingCartProvider } from "../../Context";
 import Home from "../Home";
 import MyAccount from "../MyAccount";
 import MyOrders from "../MyOrders";
@@ -12,23 +13,24 @@ import Navbar from "../../Components/Navbar";
 
 const AppRoutes = () => {
   let routes = useRoutes([
-    {path: "/", element: <Home />},
-    {path: "/my-account", element: <MyAccount />},
-    {path: "/my-order", element: <MyOrder />},
-    {path: "/my-orders", element: <MyOrders />},
-    {path: "/sign-in", element: <SignIn/>},
-    {path: "/*", element: <NotFound/>},
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "/*", element: <NotFound /> },
   ]);
   return routes;
 };
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <Navbar/>
-      <AppRoutes />
-      
-    </BrowserRouter>
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <Navbar />
+        <AppRoutes />
+      </BrowserRouter>
+    </ShoppingCartProvider>
   );
 };
 
