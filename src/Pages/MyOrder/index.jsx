4be1081a93a858ff5179b4eab19d78b95cdf244a
nelsonvilte <nelsonvilte@gmail.com>
2024-06-23 12:26:31 -0300

    update purchase order styles

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -19,7 +19,7 @@ function MyOrder() {
         <Link to={"/my-orders"} className="absolute left-0">
           <ChevronLeftIcon className="h-6 w-6 text-black cursor-pointer" />
         </Link>
-        <h1>My Order</h1>
+        <h1 className="font-medium text-xl">My Order</h1>
       </div>
       <div className="flex flex-col w-80">
         {context.order?.[index]?.products.map((product) => (
