5c4793e8c4e96d9d5dd298e3c603dac2d424f740
nelsonvilte <nelsonvilte@gmail.com>
2024-06-23 14:10:34 -0300

    refactor code and add add products search input

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,50 +1,67 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-export const ShoppingCartContext = createContext()
+export const ShoppingCartContext = createContext();
 
 // eslint-disable-next-line react/prop-types
-export const ShoppingCartProvider = ({children})=>{
-    // shopping Cart- Increment quantity
-    const [count, setCount]=useState(0)
+export const ShoppingCartProvider = ({ children }) => {
+  // shopping Cart- Increment quantity
+  const [count, setCount] = useState(0);
 
-    //Shopping Cart - Add products to cart
-    const [cartProducts, setCartProducts]=useState([])
+  //Shopping Cart - Add products to cart
+  const [cartProducts, setCartProducts] = useState([]);
 
-    //Shopping cart - Order
-    const [order, setOrder]=useState([])
+  //Shopping cart - Order
+  const [order, setOrder] = useState([]);
 
-    // product detail - open/close
-    const [isProductDetailOpen, setIsProductDetailOpen]=useState(false)
-    const openProductDetail=()=>setIsProductDetailOpen(true)
-    const closeProductDetail=()=>setIsProductDetailOpen(false)
+  // product detail - open/close
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+  const openProductDetail = () => setIsProductDetailOpen(true);
+  const closeProductDetail = () => setIsProductDetailOpen(false);
 
-        // Checkout side menu - open/close
-        const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen]=useState(false)
-        const openCheckoutSideMenu=()=>setIsCheckoutSideMenuOpen(true)
-        const closeCheckoutSideMenu=()=>setIsCheckoutSideMenuOpen(false)
-    
+  // Checkout side menu - open/close
+  const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false);
+  const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true);
+  const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false);
 
-    // producto detail - show product
-    const [productToShow, setProductToShow]=useState({})
+  // producto detail - show product
+  const [productToShow, setProductToShow] = useState({});
 
-    return(
-        <ShoppingCartContext.Provider value={{
-            count,
-            setCount,
-            openProductDetail,
-            closeProductDetail,
-            isProductDetailOpen,
-            productToShow, 
-            setProductToShow,
-            cartProducts,
-            setCartProducts,
-            isCheckoutSideMenuOpen, 
-            openCheckoutSideMenu,
-            closeCheckoutSideMenu,
-            order, 
-            setOrder
-        }}>
-            {children}
-        </ShoppingCartContext.Provider>
-    )
-}
+  //Get products
+  const [items, setItems] = useState(null);
+    //Get products by title
+  const [searchByTitle, setSearchByTitle]= useState(null)
+  console.log('searchByTitle: ',searchByTitle )
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        count,
+        setCount,
+        openProductDetail,
+        closeProductDetail,
+        isProductDetailOpen,
+        productToShow,
+        setProductToShow,
+        cartProducts,
+        setCartProducts,
+        isCheckoutSideMenuOpen,
+        openCheckoutSideMenu,
+        closeCheckoutSideMenu,
+        order,
+        setOrder,
+        items,
+        setItems,
+        searchByTitle, 
+        setSearchByTitle
+      }}
+    >
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+};
